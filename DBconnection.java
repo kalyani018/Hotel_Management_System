4b718a461cea72b5ea9dc914f59a2624a5bc38da
Aitipamula Kalyani <aitipamulakalyani@gmail.com>
2026-06-02 14:10:14 +0530

    Add utility package files

---
+++ DBconnection.java
@@ -0,0 +1,26 @@
+package hotelManagentSystem.utility;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBconnection {
+
+	private static Connection con;
+
+	public static Connection getConnection() {
+
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/hotel_management", "root", "root");
+
+
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return con;
+	}
+
+}
